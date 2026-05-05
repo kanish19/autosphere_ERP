56145613dd2c4e4f96a72467a341889953bf53bd
vinayvp5 <hsvinayprasad5@gmail.com>
2026-05-05 15:15:57 +0530

    db

--- BE/src/server.js
+++ BE/src/server.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+const dotenv = require('dotenv');   // ✅ ADD THIS
+const connectDB = require('./config/db');
+
+// Load env variables
+dotenv.config();                    // ✅ VERY IMPORTANT (MUST BE BEFORE DB)
 
 const app = express();
+
+// Connect to DB
+connectDB();
 
 // Enable CORS
 app.use(cors());
